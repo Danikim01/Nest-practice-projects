4f4d6055f3a2af9a2e4914548678a02dcc79cba9
Danikim01 <dakim@fi.uba.ar>
2025-07-24 11:35:57 -0300

    register & login implemented (jwt web token pending)

--- teslo-shop/src/auth/auth.module.ts
+++ teslo-shop/src/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User, Auth } from './entities';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User, Auth])],
   controllers: [AuthController],
   providers: [AuthService],
 })
---
+++ teslo-shop/src/auth/entities/index.ts
@@ -0,0 +1,2 @@
+export { User } from './user.entity';
+export { Auth } from './auth.entity';

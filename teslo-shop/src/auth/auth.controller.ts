40316c999c91b1442a4aa707800422317efc7b2d
Danikim01 <dakim@fi.uba.ar>
2025-07-25 10:59:06 -0300

    user role guard and role protecteed decorator implemented

--- teslo-shop/src/auth/auth.controller.ts
+++ teslo-shop/src/auth/auth.controller.ts
@@ -5,6 +5,8 @@ import { AuthGuard } from '@nestjs/passport';
 import { GetUser, GetRawHeaders } from './decorators';
 import { User } from './entities';
 import { UserRoleGuard } from './guards/user-role/user-role.guard';
+import { ValidRoles } from './interfaces';
+import { RoleProtected } from './decorators/role-protected/role-protected.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -36,7 +38,7 @@ export class AuthController {
   }
 
   @Get('private2')
-  @SetMetadata('roles', ['admin', 'super-user'])
+  @RoleProtected(ValidRoles.admin, ValidRoles.superUser)
   @UseGuards(AuthGuard(), UserRoleGuard)
   testingPrivateRoute2(@GetUser() user: User) {
     return { message: 'This is a private route 2', user };
--- teslo-shop/src/auth/guards/user-role/user-role.guard.ts
+++ teslo-shop/src/auth/guards/user-role/user-role.guard.ts
@@ -1,6 +1,7 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable, BadRequestException, ForbiddenException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
+import { META_ROLES } from '../../decorators/role-protected/role-protected.decorator';
 
 @Injectable()
 export class UserRoleGuard implements CanActivate {
@@ -10,10 +11,21 @@ export class UserRoleGuard implements CanActivate {
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     
-    const validRoles: string[] = this.reflector.get('roles', context.getHandler());
+    const validRoles: string[] = this.reflector.get(META_ROLES, context.getHandler());
     
-    console.log(validRoles);
+    const req = context.switchToHttp().getRequest();
+    const user = req.user
+  
+    if (!user) throw new BadRequestException('User not found');
 
-    return true;
+    if (validRoles.length === 0 || !validRoles) return true;
+
+    for (const role of user.roles) {
+      if (validRoles.includes(role)) return true;
+    }
+
+    throw new ForbiddenException(
+      `User ${user.fullName} needs a valid role: ${validRoles.join(', ')}`,
+    );
   }
 }

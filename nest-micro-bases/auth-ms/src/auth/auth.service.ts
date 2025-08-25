b96846c877c6b9199760a59443048a465a320c7d
Danikim01 <dakim@fi.uba.ar>
2025-08-25 11:32:16 -0300

    auth-ms register implemented

--- nest-micro-bases/auth-ms/src/auth/auth.service.ts
+++ nest-micro-bases/auth-ms/src/auth/auth.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit, Logger, HttpStatus } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { RpcException } from '@nestjs/microservices';
+import { RegisterUserDto } from 'src/dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('AuthService');
+
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('MongoDB Database connected');
+  }
+
+  async registerUser(registerUserDto: RegisterUserDto) {
+    const { name, email, password } = registerUserDto;
+    try {
+      const user = await this.user.findUnique({ where: { email } });
+      if (user) {
+        throw new RpcException({
+          status: HttpStatus.BAD_REQUEST,
+          message: 'User already exists',
+        });
+      }
+      const newUser = await this.user.create({
+        data: {
+          name,
+          email,
+          password,
+        },
+      });
+      return newUser;
+    } catch (error) {
+      throw new RpcException({
+        status: HttpStatus.BAD_REQUEST,
+        message: error.message,
+      });
+    }
+  }
+}
--- nest-micro-bases/client-gateway/src/auth/auth.controller.ts
+++ nest-micro-bases/client-gateway/src/auth/auth.controller.ts
@@ -1,8 +1,9 @@
 import { Controller, Post, Get, Body } from '@nestjs/common';
 import { Inject } from '@nestjs/common';
 import { NATS_SERVICE } from 'src/config/services';
-import { ClientProxy } from '@nestjs/microservices';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { LoginUserDto, RegisterUserDto } from './dto';
+import { catchError } from 'rxjs';
 
 @Controller('auth')
 export class AuthController {
@@ -10,16 +11,28 @@ export class AuthController {
 
   @Post('register')
   registerUser(@Body() registerUserDto: RegisterUserDto) {
-    return this.client.send('auth.register.user', registerUserDto);
+    return this.client.send('auth.register.user', registerUserDto).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
   }
 
   @Post('login')
   loginUser(@Body() loginUserDto: LoginUserDto) {
-    return this.client.send('auth.login.user', loginUserDto);
+    return this.client.send('auth.login.user', loginUserDto).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
   }
 
   @Get('verify-token')
   verifyToken() {
-    return this.client.send('auth.verify.token', {});
+    return this.client.send('auth.verify.token', {}).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
   }
 }
